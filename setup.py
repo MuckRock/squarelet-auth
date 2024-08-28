75cb1c5959855abd1957c87dd6f7732c3ea25382
Mitchell Kotler <mitch@mitchellkotler.net>
2024-08-28 15:17:34 -0400

    bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="squarelet_auth",  # Replace with your own username
-    version="0.1.4",
+    version="0.1.5",
     author="Mitchell Kotler",
     author_email="mitch@muckrock.com",
     description="Django authentication against the MuckRock user service",
